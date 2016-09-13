7367ecf8576252c3f08fc70fd58b1242fbee6844
yangshuyu <810043299@qq.com>
2016-09-13 12:11:11 +0800

    又增加一个

--- MyApplication/app/src/main/java/com/example/admin/yangyangyang/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/admin/yangyangyang/MainActivity.java
@@ -13,5 +13,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         TextView textView = new TextView(this);
+
+        textView.setText("nihao");
     }
 }
